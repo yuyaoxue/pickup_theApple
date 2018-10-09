56788aefc8abdc60a3e1d87e44df163acf3ba30d
Yuyao <972934652@qq.com>
2018-10-09 18:39:59 +0800

    让篮筐跟随鼠标移动

--- Assets/scripts/Basket.cs
+++ Assets/scripts/Basket.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
+    void Update()
+    {
+        // 从 Input 中获取鼠标在屏幕中的当前位置
+        Vector3 mousePos2D = Input.mousePosition;
+        // 摄像机的 z 坐标决定在三维空间中将鼠标沿 z 轴向前移动多远
+        mousePos2D.z = -Camera.main.transform.position.z;
+        // 将该点从二维屏幕空间转换为三维游戏世界空间
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //将篮筐的 x 位置移动到鼠标处的 x 位置处
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
 
-	// Use this for initialization
-	void Start () 
-	{
-		
-	}
 }
