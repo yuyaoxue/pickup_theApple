c2dca338a1ce897a7bf7ef137808a9b680ef8a93
Yuyao <972934652@qq.com>
2018-10-09 18:21:50 +0800

    add base code in appleTree

--- Assets/scripts/AppleTree.cs
+++ Assets/scripts/AppleTree.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    // 用来初始化苹果实例的预设
+    public GameObject applePrefab;
+    // 苹果树移动的速度，单位 米/秒
+    public float speed = 1f;
+    // 苹果树的活动区域，到达边界时则改变方向
+    public float leftAndRightEdge = 10;
+    // 苹果出现的时间间隔
+    public float secondsBetweenAppleDrops = 1f;
+    // 苹果树改变方向的概率
+    private float chanceToChange1Directions = 0.01f;
 
 	// Use this for initialization
 	void Start () 
 	{
-		
 	}
+
+    private void Update()
+    {
+        // 基本运动
+
+
+        //改变方向
+
+    }
+
 }
