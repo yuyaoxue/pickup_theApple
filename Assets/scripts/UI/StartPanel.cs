1979b45dc58038a0865ec5829cbd7dd64faebb6f
Yuyao <972934652@qq.com>
2018-10-11 21:17:48 +0800

    添加开始界面

---
+++ Assets/scripts/UI/StartPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPanel : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+    public void BtnStartClickHandle()
+    {
+        SceneManager.LoadScene(GameConst.scene_inGame);
+    }
+}
